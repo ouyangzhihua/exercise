e1189e9fe7b06098eca391f146322e39bfa181cd
ouyangzhihua <1987430106@qq.com>
2021-03-26 22:28:38 +0800

    complete bibarytree

---
+++ src/demo/Stack.java
@@ -0,0 +1,36 @@
+package demo;
+import java.util.*;
+
+public class Stack 
+{
+	
+	class CQueue 
+	{
+		/* 剑指 Offer 09. 用两个栈实现队列
+		 * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+		 * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+		 * 
+		 * 思路：
+		 * */
+
+	    public CQueue() 
+	    {
+	    	Deque<Integer> stack1;
+	    	Deque<Integer> stack2; 
+	    }
+	    
+	    public void appendTail(int value) {
+
+	    }
+	    
+	    public int deleteHead() {
+
+	    }
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
