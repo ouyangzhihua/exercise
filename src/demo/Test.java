924e245fa51e1374acdc0c58750131a6b81a279a
ouyangzhihua <1987430106@qq.com>
2021-05-04 20:07:25 +0800

    update

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -5,9 +5,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Detail d = new Detail();
-		String s = d.printNumbers(5);
-		System.out.print(s);
+		int[][] arr = {{1}};
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
 	}
 
 }
